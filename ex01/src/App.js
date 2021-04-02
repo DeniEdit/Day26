9dd269783ae7982d57de6dc1a5ddeccd867d1840
DeniEdit <milicevic.deni@gmail.com>
2021-04-02 17:40:07 +0200

    recovered files

---
+++ ex01/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import GetRequest from './GetRequest';
+
+function App() {
+  return (
+    <div className="App">
+      {
+        <GetRequest />
+      }
+    </div>
+  );
+}
+
+export default App;
